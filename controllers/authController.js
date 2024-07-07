9614bbd8f58100e53c556b3fee4278a9d42c4974
Jon-Felipe <rodriguesjf7@gmail.com>
2024-07-07 17:10:49 +0200

    validating register route body values

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,15 @@
 import User from '../models/UserModel.js';
+import { validationResult } from 'express-validator';
 import { BadRequestError } from '../errors/customErrors.js';
 
 export async function register(req, res, next) {
-  const { email } = req.body;
-  const userExists = await User.findOne({ email });
-  if (userExists) {
-    throw new BadRequestError('email already exists');
-  } else {
-    await User.create(req.body);
-    res.status(201).json({ msg: 'user created' });
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    const errorMessage = result.array().map((error) => error.msg);
+    throw new BadRequestError(errorMessage);
   }
+  await User.create(req.body);
+  res.status(201).json({ msg: 'user created' });
 }
 
 export function login(req, res) {
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,35 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import User from '../models/UserModel.js';
+import { BadRequestError } from '../errors/customErrors.js';
 import { register, login } from '../controllers/authController.js';
 const router = Router();
 
-router.post('/register', register);
+router.post(
+  '/register',
+  body('firstName').trim().notEmpty().withMessage('First name is required'),
+  body('lastName').trim().notEmpty().withMessage('Last name is required'),
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Invalid email format')
+    .custom(async (email) => {
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        throw new BadRequestError('Email already exists');
+      }
+    })
+    .normalizeEmail(),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters'),
+  register
+);
 router.post('/login', login);
 
 export default router;
