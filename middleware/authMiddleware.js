c50cf70871133fb6d9fd9732eb9b1eb1b7fe3032
Jon-Felipe <rodriguesjf7@gmail.com>
2024-07-10 07:41:47 +0200

    auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+
+export function authenticateUser(req, res, next) {
+  const { token } = req.cookies;
+
+  if (!token) {
+    throw new Error('Authentication invalid');
+  }
+  try {
+    const { userId } = jwt.decode(token, 'secret');
+    req.user = userId;
+    next();
+  } catch (error) {
+    throw new Error('Authentication invalid');
+  }
+}
