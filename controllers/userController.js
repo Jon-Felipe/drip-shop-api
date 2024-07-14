8cfb21f3364628fdb8b47922bfdeae2fe57169bf
Jon-Felipe <rodriguesjf7@gmail.com>
2024-07-14 17:23:26 +0200

    update user route initial

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,3 +4,7 @@ export async function getCurrentUser(req, res) {
   const user = await User.findOne({ _id: req.user.userId }).select('-password');
   res.status(200).json({ user });
 }
+
+export async function updateUser(req, res) {
+  res.send('update user');
+}
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getCurrentUser } from '../controllers/userController.js';
+import { getCurrentUser, updateUser } from '../controllers/userController.js';
 import { authenticateUser } from '../middleware/authMiddleware.js';
 const router = Router();
 
 router.get('/current-user', authenticateUser, getCurrentUser);
+router.patch('/update-user', authenticateUser, updateUser);
 
 export default router;
