56d6f7cf6b32989917e69ae7feb8be6b04831c6d
Jon-Felipe <rodriguesjf7@gmail.com>
2024-08-19 20:03:20 +0200

    update product in db

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,7 +16,12 @@ export async function createProduct(req, res) {
 }
 
 export async function updateProduct(req, res) {
-  res.send('update product');
+  const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: 'true' }
+  );
+  res.status(200).json({ product: updatedProduct });
 }
 
 export async function deleteProduct(req, res) {
