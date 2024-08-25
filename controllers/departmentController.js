f312407f936083c6fbeba1c9051eee8a57ebb185
Jon-Felipe <rodriguesjf7@gmail.com>
2024-08-25 10:47:49 +0200

    get department and create department functionality

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -1,9 +1,14 @@
-// import Department from '../models/DepartmentModel.js';
+import Department from '../models/DepartmentModel.js';
 
 export async function getDepartment(req, res) {
-  res.send('get department');
+  const { department } = req.params;
+  const foundDepartment = await Department.findOne({
+    name: { $regex: department, $options: 'i' },
+  });
+  res.status(200).json({ department: foundDepartment });
 }
 
 export async function createDepartment(req, res) {
-  res.send('create department');
+  const department = await Department.create(req.body);
+  res.status(201).json({ department });
 }
