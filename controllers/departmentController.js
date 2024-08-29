c2989ff09ee3f0cc365cf23a449482ca4f61df43
Jon-Felipe <rodriguesjf7@gmail.com>
2024-08-29 19:22:49 +0200

    return products that belong to queried department

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -2,10 +2,20 @@ import Department from '../models/DepartmentModel.js';
 
 export async function getDepartment(req, res) {
   const { department } = req.params;
-  const foundDepartment = await Department.findOne({
-    name: { $regex: department, $options: 'i' },
-  });
-  res.status(200).json({ department: foundDepartment });
+  const foundDepartment = await Department.aggregate([
+    {
+      $lookup: {
+        from: 'products',
+        localField: '_id',
+        foreignField: 'department',
+        as: 'products',
+      },
+    },
+    {
+      $match: { name: { $regex: `^${department}$`, $options: 'i' } },
+    },
+  ]);
+  res.status(200).json({ department: foundDepartment[0] });
 }
 
 export async function createDepartment(req, res) {
