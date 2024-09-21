660d6c9d783f03ed10a7a0613c38394e9cb1f53f
Jon-Felipe <rodriguesjf7@gmail.com>
2024-09-21 11:18:57 +0200

    not found custom error

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -1,3 +1,11 @@
+export class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NotFoundError';
+    this.statusCode = 404;
+  }
+}
+
 export class BadRequestError extends Error {
   constructor(message) {
     super(message);
