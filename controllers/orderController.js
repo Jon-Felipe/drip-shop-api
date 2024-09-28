bfd196f77b4217193680c91af563f01d514b3606
Jon-Felipe <rodriguesjf7@gmail.com>
2024-09-28 10:53:12 +0200

    create orders for authenticated user

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,22 @@
+import Order from '../models/OrderModel.js';
+
 export async function createOrder(req, res) {
-  res.send('create order');
+  const {
+    orderItems,
+    shippingAddress,
+    shippingMethod,
+    paymentMethod,
+    totalPrice,
+  } = req.body;
+  const order = await Order.create({
+    user: req.user.userId,
+    orderItems,
+    shippingAddress,
+    shippingMethod,
+    paymentMethod,
+    totalPrice,
+  });
+  res.status(200).json({ order });
 }
 
 export async function getOrders(req, res) {
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { getOrders, createOrder } from '../controllers/orderController.js';
+import { authenticateUser } from '../middleware/authMiddleware.js';
 const router = Router();
 
-router.route('/').get(getOrders).post(createOrder);
+router.route('/').get(getOrders).post(authenticateUser, createOrder);
 
 export default router;
